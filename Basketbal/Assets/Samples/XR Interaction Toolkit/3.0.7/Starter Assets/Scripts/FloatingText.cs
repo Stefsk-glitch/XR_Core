070809a55ae1aaffe947475763529a065e5d9e03
GertieMeneer <gertjan.vat@icloud.com>
2025-03-25 11:29:20 +0100

    fix text keeps moving in front of player

--- Basketbal/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/FloatingText.cs
+++ Basketbal/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/FloatingText.cs
@@ -3,24 +3,26 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     Transform mainCam;
-    Transform unit;
     Transform worldSpaceCanvas;
 
-    public Vector3 offset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float distanceFromPlayer = 2.0f;
+
     void Start()
     {
         mainCam = Camera.main.transform;
-        unit = transform.parent;
-        worldSpaceCanvas = GameObject.FindObjectOfType<Canvas>().transform;
 
+        worldSpaceCanvas = GameObject.FindObjectOfType<Canvas>().transform;
         transform.SetParent(worldSpaceCanvas);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = unit.position + offset;
-        transform.rotation = Quaternion.LookRotation(transform.position - mainCam.position);
+        Vector3 forwardOffset = mainCam.forward * distanceFromPlayer;
+        transform.position = mainCam.position + forwardOffset;
+
+        Vector3 cameraForward = mainCam.forward;
+        cameraForward.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(cameraForward);
+        transform.rotation = lookRotation;
     }
 }
