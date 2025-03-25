0b592365bf891591087cc270a7285c45170b739a
GertieMeneer <gertjan.vat@icloud.com>
2025-03-25 12:02:40 +0100

    Add verticaloffset, texts don't overlap

--- Basketbal/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/FloatingText.cs
+++ Basketbal/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/FloatingText.cs
@@ -6,6 +6,7 @@ public class FloatingText : MonoBehaviour
     Transform worldSpaceCanvas;
 
     public float distanceFromPlayer = 2.0f;     // offset from player
+    public float verticalOffset = 0f;
 
     void Start()
     {
@@ -18,7 +19,8 @@ public class FloatingText : MonoBehaviour
     void Update()
     {
         Vector3 forwardOffset = mainCam.forward * distanceFromPlayer;
-        transform.position = mainCam.position + forwardOffset;
+        Vector3 verticalOffsetVector = Vector3.up * verticalOffset;
+        transform.position = mainCam.position + forwardOffset + verticalOffsetVector;
 
         Vector3 cameraForward = mainCam.forward;
         cameraForward.y = 0;                    // keeps text upright
