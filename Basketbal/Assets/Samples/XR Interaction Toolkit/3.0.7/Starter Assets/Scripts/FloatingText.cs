dd21266d7bd28071b5b2b67fc8dcfc6e425eee13
GertieMeneer <gertjan.vat@icloud.com>
2025-03-31 22:41:43 +0200

    Little refactoring`

--- Basketbal/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/FloatingText.cs
+++ Basketbal/Assets/Samples/XR Interaction Toolkit/3.0.7/Starter Assets/Scripts/FloatingText.cs
@@ -3,25 +3,21 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     Transform mainCam;
-    Transform worldSpaceCanvas;
 
     public float distanceFromPlayer = 2.0f;     // offset from player
     public float verticalOffset = 0f;
-    public float horizontalOffset = -4f;         // New variable for horizontal offset
+    public float horizontalOffset = -4f;
 
     void Start()
     {
         mainCam = Camera.main.transform;
-
-        worldSpaceCanvas = GameObject.Find("Score Canvas").transform;
-        transform.SetParent(worldSpaceCanvas);
     }
 
     void Update()
     {
         Vector3 forwardOffset = mainCam.forward * distanceFromPlayer;
         Vector3 verticalOffsetVector = Vector3.up * verticalOffset;
-        Vector3 horizontalOffsetVector = mainCam.right * horizontalOffset;  // New horizontal offset
+        Vector3 horizontalOffsetVector = mainCam.right * horizontalOffset;
 
         transform.position = mainCam.position + forwardOffset + verticalOffsetVector + horizontalOffsetVector;
 
